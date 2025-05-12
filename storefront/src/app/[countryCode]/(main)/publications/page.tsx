963977394e2d0b45571d8a670cd074a888262f49
joeylynchhh <joseph.lynch.dr@gmail.com>
2025-05-12 10:50:41 -0700

    I believe this is the sixth attempt. Slow and steady.

--- storefront/src/app/[countryCode]/(main)/publications/page.tsx
+++ storefront/src/app/[countryCode]/(main)/publications/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './publications.module.css';
+import styles from './publication.module.css';
 
 export default function PublicationsPage() {
   return (
