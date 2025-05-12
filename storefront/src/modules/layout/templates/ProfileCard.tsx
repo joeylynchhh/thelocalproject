95e60dbb9e4da9b324c51ae8a278309ce7f09b73
joeylynchhh <joseph.lynch.dr@gmail.com>
2025-05-12 09:40:28 -0700

    Fourth attempt to resolve build errors.

--- storefront/src/modules/layout/templates/ProfileCard.tsx
+++ storefront/src/modules/layout/templates/ProfileCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from '@/styles/ProfileCard.module.css';
+import styles from './ProfileCard.module.css';
 
 interface ProfileProps {
   profile: {
