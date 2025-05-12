8ae5f3d0223fc686268b1a02960a8a4b285acd44
joeylynchhh <joseph.lynch.dr@gmail.com>
2025-05-12 10:54:21 -0700

    I believe this is the seventh attempt. Fixing a small typo.  Slow and steady.

--- storefront/src/app/[countryCode]/(main)/publications/page.tsx
+++ storefront/src/app/[countryCode]/(main)/publications/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './publication.module.css';
+import styles from './publications.module.css';
 
 export default function PublicationsPage() {
   return (
